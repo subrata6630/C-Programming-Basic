d423c9ea83e5c0d8af6e3db74c51debd4561189f
Subrata Deb Nath <subrata6630@gmail.com>
2017-03-21 23:53:02 +0600

    c

---
+++ 15 nesting loop part 1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+// Alim Ul Karim
+// swapping
+// nesting loops behaviour
+// reversing
+// sorting
+
+void swapping(int num[], int index1, int index2){
+}
+void reverse_array(int num[], int size){
+}
+
+int main() {
+
+
+    getchar();
+    return 0;
+}
+
+
+
